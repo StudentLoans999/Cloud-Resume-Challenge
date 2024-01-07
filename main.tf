282c0eb47024b7ed33b89bc3849f0d4e693f17d4
David Richey <davidsacc@gmail.com>
2024-01-07 22:32:11 +0000

    Fixed the bucket polict to match ClickOps

--- main.tf
+++ main.tf
@@ -38,30 +38,23 @@ resource "aws_s3_bucket_acl" "CRC_bucket" {
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+resource "aws_s3_bucket_policy" "CRC_bucket_bucket_policy" {
   bucket = aws_s3_bucket.CRC_bucket.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid = "PublicRead"
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.CRC_bucket.arn}/*"
+      }
+    ]
+  })
 }
 
-data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement: {
-
-    sid: { "PublicRead"
-    }
-
-    principals {
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "s3:GetObject",
-    ]
-
-    resources = [
-      "${aws_s3_bucket.CRC_bucket.arn}/*",
-    ]
-  }
-}
 
 resource "aws_s3_bucket" "CRC_bucket" {
   bucket = "terraformbucket.org"
