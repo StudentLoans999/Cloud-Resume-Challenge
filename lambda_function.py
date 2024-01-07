db6fbadd9f97f0a84bf8bfbe2dc7445f2dad99db
David Richey <davidsacc@gmail.com>
2024-01-07 15:46:14 -0500

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,17 @@
 import boto3
+import json
 
-dynamodb = boto3.resouce('dynamodb')
-table = dynamodb.Table('Terraform-Table-CRC')
+def lambda_handler(event, context):
+  dynamodb = boto3.resouce('dynamodb')
+  table = dynamodb.Table('Terraform-Table-CRC')
 
-response = table.update_item(Key={ "id":"1"}, 
+  response = table.update_item(Key={ "id":"1"}, 
                   UpdateExpression='SET view_count = view_count + :incr', 
                   ExpressionAttributeValues={':incr': {"N": "1"}} 
                   )
-return response
+  return response
+  
+  return {
+        'statusCode': 200,
+        'body': json.dumps('Function Executed Successfully!')
+    }
