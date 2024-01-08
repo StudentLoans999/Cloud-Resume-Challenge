aef251977c1caa20a93f4a9e7583f4775bc367ca
David Richey <davidsacc@gmail.com>
2024-01-08 05:48:25 +0000

    Forgot to delete this line

--- main.tf
+++ main.tf
@@ -167,8 +167,6 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_r53_validation : record.fqdn]
-  subject_alternative_names = ["*.davidrichey.org"]  # Include if you want to cover subdomains
-
 }
 
 resource "aws_route53_record" "cert_r53_validation" {
