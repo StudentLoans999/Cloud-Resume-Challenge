ea33771cd6fb682ec7cbe8dddbcc2804ffb7adda
David Richey <davidsacc@gmail.com>
2024-01-08 16:04:27 +0000

    Fixed lamba function

--- lambda_function.py
+++ lambda_function.py
@@ -2,16 +2,22 @@ import boto3
 import json
 
 def lambda_handler(event, context):
-  dynamodb = boto3.resource('dynamodb')
-  table = dynamodb.Table('Terraform-Table-CRC')
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Terraform-Table-CRC')
 
-  response = table.update_item(Key={ "id":"1"}, 
-                  UpdateExpression='SET view_count = view_count + :incr', 
-                  ExpressionAttributeValues={':incr': {"N": "1"}} 
-                  )
-  return response
-  
-  return {
+    # Update the item in DynamoDB table
+    response = table.update_item(
+        Key={"id": "1"},
+        UpdateExpression='SET view_count = view_count + :incr',
+        ExpressionAttributeValues={':incr': 1},
+        ReturnValues="UPDATED_NEW"
+    )
+
+    # Construct and return the response
+    return {
         'statusCode': 200,
-        'body': json.dumps('Function Executed Successfully!')
-  }
+        'body': json.dumps({
+            'message': 'Function Executed Successfully!',
+            'dynamodbResponse': response
+        })
+    }
