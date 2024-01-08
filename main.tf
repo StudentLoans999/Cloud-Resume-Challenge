5a98d7b7913473531f334ed769635d6b3adc17db
David Richey <davidsacc@gmail.com>
2024-01-08 03:01:53 +0000

    Fixed Certificate Validation to use a Dynamic Reference

--- main.tf
+++ main.tf
@@ -165,7 +165,7 @@ resource "aws_acm_certificate" "cert" {
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 resource "aws_route53_record" "cert_validation" {
