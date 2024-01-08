d1bcdeb9b006d2d83f20e1ec8ce34eef90ae414d
David Richey <davidsacc@gmail.com>
2024-01-08 05:57:49 +0000

    Not original yet

--- main.tf
+++ main.tf
@@ -163,14 +163,6 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "davidrichey.org"
-  validation_method = "DNS"
-  subject_alternative_names = ["*.davidrichey.org"]  # Include if you want to cover subdomains
-
-  // ... other configuration ...
-}
-
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
