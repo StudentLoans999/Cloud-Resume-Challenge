ee4fe4457940023d94566f804f27e7d9a35b89dd
David Richey <davidsacc@gmail.com>
2024-01-08 16:22:24 +0000

    Deleteed subdomain_a_record. Updated Lambda to include if_not_exists

--- lambda_function.py
+++ lambda_function.py
@@ -5,11 +5,11 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Terraform-Table-CRC')
 
-    # Update the item in DynamoDB table
+    # Update the item in DynamoDB table (if_not_exists sets view_count to 0 if it doesn't exist AKA being run for the first time)
     response = table.update_item(
         Key={"id": "1"},
-        UpdateExpression='SET view_count = view_count + :incr',
-        ExpressionAttributeValues={':incr': 1},
+        UpdateExpression='SET view_count = if_not_exists(view_count, :start) + :incr',
+        ExpressionAttributeValues={':incr': 1, ':start': 0},
         ReturnValues="UPDATED_NEW"
     )
 
--- main.tf
+++ main.tf
@@ -200,18 +200,6 @@ resource "aws_route53_record" "cloudfront_alias" {
   }
 }
 
-resource "aws_route53_record" "subdomain_a_record" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "*.davidrichey.org" 
-  type    = "A"
-
-  alias {
-    name = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
-
 resource "aws_route53_record" "a_record" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "davidrichey.org" 
