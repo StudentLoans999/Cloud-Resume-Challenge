64cf3c604c0cfa7cd21e762c9f3ed64c2bfa0876
David Richey <davidsacc@gmail.com>
2024-01-08 06:10:54 +0000

    Trying to back to original

--- main.tf
+++ main.tf
@@ -232,6 +232,17 @@ resource "aws_route53_record" "cname" {
   records = [aws_cloudfront_distribution.s3_distribution.domain_name]
 }
 
+resource "aws_route53_record" "subdomain_a_record" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "*.davidrichey.org" 
+  type    = "A"
+
+  alias {
+    name = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
